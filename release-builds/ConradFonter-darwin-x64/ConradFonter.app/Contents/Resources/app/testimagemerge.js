eb1fff680eedd702f502e81299cd2a707f287594
Vasista <vasista@Macbook.local>
2018-05-13 13:16:44 -0400

    remove release-builds

--- release-builds/ConradFonter-darwin-x64/ConradFonter.app/Contents/Resources/app/testimagemerge.js
+++
@@ -1,7 +0,0 @@
-const mergeImg = require('merge-img');
-
-mergeImg(['letters/A.png', 'letters/B.png'])
-  .then((img) => {
-    // Save image as file
-    img.write('temp/out.png', () => console.log('done'));
-  });
--- release-builds/ConradFonter-darwin-x64/ConradFonter.app/Contents/Resources/app/testnotification.js
+++
@@ -1,6 +0,0 @@
-const electron = require('electron');
-
-
-const {Notification} = electron;
-
-var a = new Notification([{title: 'Text Rendered!', body: 'hi'}]).show();
